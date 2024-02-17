0a083fbfba77569b8413b84007896f43f1825342
Rodrigo Salinas <salinas.e.rodrigo@gmail.com>
2024-02-17 04:33:48 -0300

    actualización a los settings.py

--- DonOnofre/DonOnofre/settings.py
+++ DonOnofre/DonOnofre/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-mb-d+c@91od8*zfuzv%02qgbw%hcweng2mwcazq+kyb9zz=d5s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["http://18.221.248.175/"]
 
 # Application definition
 
@@ -132,5 +132,5 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
+    "http://18.221.248.175/",
 ]
